029202e79902671b94643ff2e53e590e76ad723e
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-05 19:40:28 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
-
+class CashRegister
+  attr_accessor :total 
+  
+  @items_list = []
+  
+  def initialize
+    @total = 0
+  end
+  
+  def add_item(item, price)
+    @total += price
+  end
+  
+  def apply_discount(percent)
+    
+  end
+  
+  def items
+    
+  end
+  
+  def void_last_transaction
+    
+  end
+  
+end
