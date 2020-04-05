be00e3b09c7b86b3021428f86bac434832211cde
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-05 20:09:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,7 +8,7 @@ class CashRegister
   end
   
   def add_item(item, price, quanity = 1)
-    @price = price
+    @price = price * quanity
     @total += price * quanity
     @item_list << item * quanity
   end
